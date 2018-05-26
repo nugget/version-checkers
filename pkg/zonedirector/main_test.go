26b5a6673a8c46694a25685b90c184e32d9596a3
David McNett <nugget@macnugget.org>
2018-05-26 13:19:25 -0500

    Preliminary unit tests.

---
+++ pkg/zonedirector/main_test.go
@@ -0,0 +1,23 @@
+package zonedirector
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestExpandedVersion(t *testing.T) {
+	var testTable = []struct {
+		input       string // input
+		expected    string // expected result
+		description string // what we are testing
+	}{
+		{"10.20.30.40.50", "10.20.30.40 build 5", "valid version string"},
+		{"10.20.30", "10.20.30", "Unrecognized version string"},
+	}
+
+	for _, tt := range testTable {
+		actual := ExpandedVersion(tt.input)
+		assert.Equal(t, actual, tt.expected, "Unexpected result on "+tt.description)
+	}
+}
