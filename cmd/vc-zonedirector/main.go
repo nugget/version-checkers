4da29b57604f25597f1a1c030722df408937368b
David McNett <nugget@macnugget.org>
2018-09-07 12:09:45 -0500

    Simply emit json of the latest software build.

--- cmd/vc-zonedirector/main.go
+++ cmd/vc-zonedirector/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/nugget/version-checkers/internal/vc"
 	"github.com/nugget/version-checkers/pkg/zonedirector"
 
 	"encoding/json"
@@ -13,37 +12,16 @@ func main() {
 		productID int = 73 // Default 73 "zonedirector 1200"
 	)
 
-	vc.Init()
-	state, err := vc.LoadState()
-	if err != nil {
-		panic(fmt.Sprintf("Cannot load state: %v", err))
-	}
-
-	fmt.Println("State loaded from %v", state.Timestamp)
-
 	releaseList, err := zonedirector.GetReleaseList(productID)
-	fmt.Printf("There are %d releases in the list\n", len(releaseList))
+	//fmt.Printf("There are %d releases in the list\n", len(releaseList))
 
 	latest := zonedirector.FindLatest(releaseList, "IMG")
-	fmt.Printf("%+v\n", latest)
+	//fmt.Printf("latest: %+v\n\n", latest)
 
 	json, err := json.Marshal(latest)
 	if err != nil {
 		panic(fmt.Sprintf("Cannot parse json: %v", err))
 	}
 
-	if state.IsNewer(latest.Version) {
-		fmt.Printf("NEW VERSION %s (current %s)\n", latest.Version, state.CurrentVersion)
-
-		state.RawVersion = json
-		state.CurrentVersion = latest.Version
-
-		err := state.Update()
-		if err != nil {
-			panic(fmt.Sprintf("Cannot update version: %v", err))
-		}
-	} else {
-		fmt.Printf("boring version %s (current %s)\n", latest.Version, state.CurrentVersion)
-	}
-
+	fmt.Printf("%s\n", json)
 }
